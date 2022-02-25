d60d9f0e90015fccbf836fbe4af6eaf3653687b3
KawazuHiroyuki <66836228+KawazuHiroyuki@users.noreply.github.com>
2022-02-26 02:02:20 +0900

    乱数分布クラス追加：extreme_value_distribution

--- RandomNumberGanerator/private/RandomNumberDistributionUtility.h
+++ RandomNumberGanerator/private/RandomNumberDistributionUtility.h
@@ -28,6 +28,8 @@ template <typename ResultType>
 class StdExponentialDistribution;
 template <typename ResultType>
 class StdWeibullDistribution;
+template <typename ResultType>
+class StdExtremeValueDistribution;
 
 template <typename Distribution, typename ResultType>
 using BaseDistribution =
@@ -41,7 +43,8 @@ using BaseDistribution =
     utility::Case<std::is_same<Distribution, StdPoissonDistribution<ResultType>>::value, std::poisson_distribution<ResultType>>,
     utility::Case<std::is_same<Distribution, StdExponentialDistribution<ResultType>>::value, std::exponential_distribution<ResultType>>,
     utility::Case<std::is_same<Distribution, StdExponentialDistribution<ResultType>>::value, std::gamma_distribution<ResultType>>,
-    utility::Case<std::is_same<Distribution, StdWeibullDistribution<ResultType>>::value, std::weibull_distribution<ResultType>>
+    utility::Case<std::is_same<Distribution, StdWeibullDistribution<ResultType>>::value, std::weibull_distribution<ResultType>>,
+    utility::Case<std::is_same<Distribution, StdExtremeValueDistribution<ResultType>>::value, std::extreme_value_distribution<ResultType>>
     >::type;
 
 
---
+++ RandomNumberGanerator/private/StdExtremeValueDistribution.h
@@ -0,0 +1,41 @@
+/*****************************************************************//**
+ * \file   StdExtremeValueDistribution.h
+ * \brief  乱数分布
+ *
+ * \author japan
+ * \date   December 2021
+ *********************************************************************/
+
+#pragma once
+
+ // MyProject
+#include "PrimaryRandomNumberDistribution.h"
+#include "RandomNumberDistributionUtility.h"
+
+namespace random_number_generator
+{
+/**
+ * \brief 乱数分布 - std::extreme_value_distribution
+ * \tparam DistributionResultType_ 乱数分布 生成結果の型
+ */
+template <
+    typename DistributionResultType_ = double
+>
+class StdExtremeValueDistribution : public PrimaryRandomNumberDistribution<BaseDistribution<StdExtremeValueDistribution<DistributionResultType_>, DistributionResultType_>, DistributionResultType_>
+{
+private:
+    using Base = PrimaryRandomNumberDistribution<BaseDistribution<StdExtremeValueDistribution<DistributionResultType_>, DistributionResultType_>, DistributionResultType_>;
+    using Distribution = BaseDistribution<StdExtremeValueDistribution<DistributionResultType_>, DistributionResultType_>;
+
+public:
+    /**
+     * \brief コンストラクタ
+     * \param a 極値分布の位置パラメータ
+     * \param b 極値分布の尺度パラメータ
+     */
+    StdExtremeValueDistribution(DistributionResultType_ a, DistributionResultType_ b)
+        : Base(RandomNumberDistributionID::StdExtreme, Distribution(a, b))
+    {
+    }
+};
+} // namespace random_number_generator
