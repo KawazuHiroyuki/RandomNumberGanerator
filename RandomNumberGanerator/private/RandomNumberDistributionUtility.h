1a92be5bac76821be0ee35b83032e2116349faff
KawazuHiroyuki <66836228+KawazuHiroyuki@users.noreply.github.com>
2022-02-26 01:45:54 +0900

    乱数分布クラス追加：geometric_distribution

--- RandomNumberGanerator/private/RandomNumberDistributionUtility.h
+++ RandomNumberGanerator/private/RandomNumberDistributionUtility.h
@@ -16,8 +16,8 @@ template <typename ResultType>
 class StdBernoulliDistribution;
 template <typename ResultType>
 class StdBinomialDistribution;
-//template <typename Seed>
-//class StdMt199937_64BitRandomNumberEngine;
+template <typename ResultType>
+class StdGeometricDistribution;
 //template <typename Seed>
 //class StdRanlux24RandomNumberEngine;
 //template <typename Seed>
@@ -33,8 +33,8 @@ using BaseDistribution =
     utility::Case<std::is_same<Distribution, StdUniformIntDistribution<ResultType>>::value, std::uniform_int_distribution<ResultType>>,
     utility::Case<std::is_same<Distribution, StdUniformRealDistribution<ResultType>>::value, std::uniform_real_distribution<ResultType>>,
     utility::Case<std::is_same<Distribution, StdBernoulliDistribution<ResultType>>::value, std::bernoulli_distribution>,
-    utility::Case<std::is_same<Distribution, StdBinomialDistribution<ResultType>>::value, std::binomial_distribution<ResultType>>
-    //utility::Case<std::is_same<Engine, StdMt199937_32BitRandomNumberEngine<Seed>>::value, std::mt19937>,
+    utility::Case<std::is_same<Distribution, StdBinomialDistribution<ResultType>>::value, std::binomial_distribution<ResultType>>,
+    utility::Case<std::is_same<Distribution, StdGeometricDistribution<ResultType>>::value, std::geometric_distribution<ResultType>>
     //utility::Case<std::is_same<Engine, StdMt199937_64BitRandomNumberEngine<Seed>>::value, std::mt19937_64>,
     //utility::Case<std::is_same<Engine, StdRanlux24RandomNumberEngine<Seed>>::value, std::ranlux24>,
     //utility::Case<std::is_same<Engine, StdRanlux48RandomNumberEngine<Seed>>::value, std::ranlux48>,
---
+++ RandomNumberGanerator/private/StdGeometricDistribution.h
@@ -0,0 +1,40 @@
+/*****************************************************************//**
+ * \file   StdGeometricDistribution.h
+ * \brief  乱数分布
+ *
+ * \author japan
+ * \date   December 2021
+ *********************************************************************/
+
+#pragma once
+
+ // MyProject
+#include "PrimaryRandomNumberDistribution.h"
+#include "RandomNumberDistributionUtility.h"
+
+namespace random_number_generator
+{
+/**
+ * \brief 乱数分布 - std::geometric_distribution
+ * \tparam DistributionResultType_ 乱数分布 生成結果の型
+ */
+template <
+    typename DistributionResultType_ = int
+>
+class StdGeometricDistribution : public PrimaryRandomNumberDistribution<BaseDistribution<StdGeometricDistribution<DistributionResultType_>, DistributionResultType_>, DistributionResultType_>
+{
+private:
+    using Base = PrimaryRandomNumberDistribution<BaseDistribution<StdGeometricDistribution<DistributionResultType_>, DistributionResultType_>, DistributionResultType_>;
+    using Distribution = BaseDistribution<StdGeometricDistribution<DistributionResultType_>, DistributionResultType_>;
+
+public:
+    /**
+     * \brief コンストラクタ
+     * \param p 確率
+     */
+    StdGeometricDistribution(double p)
+        : Base(RandomNumberDistributionID::StdBinomial, Distribution(p))
+    {
+    }
+};
+} // namespace random_number_generator
