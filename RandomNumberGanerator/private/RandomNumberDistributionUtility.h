2a083394ff87d22f77e9fd4645a5787e6bfc33ee
KawazuHiroyuki <66836228+KawazuHiroyuki@users.noreply.github.com>
2022-02-26 01:57:20 +0900

    乱数分布クラス追加：gamma_distribution

--- RandomNumberGanerator/private/RandomNumberDistributionUtility.h
+++ RandomNumberGanerator/private/RandomNumberDistributionUtility.h
@@ -24,8 +24,8 @@ template <typename ResultType>
 class StdPoissonDistribution;
 template <typename ResultType>
 class StdExponentialDistribution;
-//template <typename Seed>
-//class StdDefaultRandomEngine;
+template <typename ResultType>
+class StdExponentialDistribution;
 
 template <typename Distribution, typename ResultType>
 using BaseDistribution =
@@ -37,8 +37,8 @@ using BaseDistribution =
     utility::Case<std::is_same<Distribution, StdGeometricDistribution<ResultType>>::value, std::geometric_distribution<ResultType>>,
     utility::Case<std::is_same<Distribution, StdNegativeBinomialDistribution<ResultType>>::value, std::negative_binomial_distribution<ResultType>>,
     utility::Case<std::is_same<Distribution, StdPoissonDistribution<ResultType>>::value, std::poisson_distribution<ResultType>>,
-    utility::Case<std::is_same<Distribution, StdExponentialDistribution<ResultType>>::value, std::exponential_distribution<ResultType>>
-    //utility::Case<std::is_same<Engine, StdKnuthRandomNumberEngine<Seed>>::value, std::knuth_b>,
+    utility::Case<std::is_same<Distribution, StdExponentialDistribution<ResultType>>::value, std::exponential_distribution<ResultType>>,
+    utility::Case<std::is_same<Distribution, StdExponentialDistribution<ResultType>>::value, std::gamma_distribution<ResultType>>
     //utility::Case<std::is_same<Engine, StdDefaultRandomEngine<Seed>>::value, std::default_random_engine>
     >::type;
 
---
+++ RandomNumberGanerator/private/StdGammaDistribution.h
@@ -0,0 +1,41 @@
+/*****************************************************************//**
+ * \file   StdGammaDistribution.h
+ * \brief  乱数分布
+ *
+ * \author japan
+ * \date   December 2021
+ *********************************************************************/
+
+#pragma once
+
+ // MyProject
+#include "PrimaryRandomNumberDistribution.h"
+#include "RandomNumberDistributionUtility.h"
+
+namespace random_number_generator
+{
+/**
+ * \brief 乱数分布 - std::gamma_distribution
+ * \tparam DistributionResultType_ 乱数分布 生成結果の型
+ */
+template <
+    typename DistributionResultType_ = double
+>
+class StdGammaDistribution : public PrimaryRandomNumberDistribution<BaseDistribution<StdGammaDistribution<DistributionResultType_>, DistributionResultType_>, DistributionResultType_>
+{
+private:
+    using Base = PrimaryRandomNumberDistribution<BaseDistribution<StdGammaDistribution<DistributionResultType_>, DistributionResultType_>, DistributionResultType_>;
+    using Distribution = BaseDistribution<StdGammaDistribution<DistributionResultType_>, DistributionResultType_>;
+
+public:
+    /**
+     * \brief コンストラクタ
+     * \param alpha ガンマ分布の形状母数
+     * \param beta ガンマ分布の尺度母数
+     */
+    StdGammaDistribution(DistributionResultType_ alpha, DistributionResultType_ beta)
+        : Base(RandomNumberDistributionID::StdGamma, Distribution(alpha, beta))
+    {
+    }
+};
+} // namespace random_number_generator
